d1e5aaa5b53aee490ed53992e9b5f4727a7c6009
vpasias <52623837+vpasias@users.noreply.github.com>
2021-06-30 10:39:47 +0300

    Update install-software.sh

--- install-software.sh
+++ install-software.sh
@@ -7,6 +7,6 @@ cd
 sudo apt update
 sudo apt full-upgrade -y
 sudo apt install -y uvtool virtinst
-sudo uvt-simplestreams-libvirt sync release=focal arch=amd64
+#sudo uvt-simplestreams-libvirt sync release=focal arch=amd64
 sudo apt install snapd -y
 sudo snap install juju --classic
