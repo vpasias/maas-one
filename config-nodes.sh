9740bc9cb0280077bfa3847dbe088b89cb3aae5b
vpasias <52623837+vpasias@users.noreply.github.com>
2021-07-01 17:12:09 +0300

    Update config-nodes.sh

--- config-nodes.sh
+++ config-nodes.sh
@@ -24,7 +24,7 @@ for i in "${!nodeNamesMACs[@]}"; do
         maas $PROFILE machine update $SYSTEM_ID \
                 hostname=$i \
                 power_type=virsh \
-                power_parameters_power_address=qemu+ssh://ubuntu@"$KVM_INTERNAL_IP"/system \
+                power_parameters_power_address=qemu+ssh://ubuntu@10.0.0.2/system \
                 power_parameters_power_id=$i >/dev/null && echo "- Node name changed and power type configured"
         maas $PROFILE machine commission $SYSTEM_ID testing_scripts=none >/dev/null && echo "- Node commissioning (hardware tests skipped)"
 
