4982246da42c9bc8b4cf8496d68d7bbedfd42d71
vpasias <52623837+vpasias@users.noreply.github.com>
2021-08-04 16:07:30 +0300

    Update 00200-create-juju-bootstrap-controller.sh

--- os/00200-create-juju-bootstrap-controller.sh
+++ os/00200-create-juju-bootstrap-controller.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # For ubuntu 18.04 deployment
-juju bootstrap --bootstrap-series=bionic --bootstrap-constraints tags=juju maas-one maas-one && juju add-model --config default-series=bionic openstack
+juju bootstrap --bootstrap-series=bionic --bootstrap-constraints tags=juju maas-one maas-one
+juju add-model --config default-series=bionic openstack
+
 # For ubuntu 20.04 deployment
-# juju bootstrap --bootstrap-series=focal --bootstrap-constraints tags=juju maas-one maas-one && juju add-model --config default-series=focal openstack
+# juju bootstrap --bootstrap-series=focal --bootstrap-constraints tags=juju maas-one maas-one
+# juju add-model --config default-series=focal openstack
