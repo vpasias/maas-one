4978e0afcf7ba8ca6a0c960ec906c2ad611a31b1
vpasias <52623837+vpasias@users.noreply.github.com>
2021-08-04 16:05:56 +0300

    Update 00200-create-juju-bootstrap-controller.sh

--- os/00200-create-juju-bootstrap-controller.sh
+++ os/00200-create-juju-bootstrap-controller.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # For ubuntu 18.04 deployment
 juju bootstrap --bootstrap-series=bionic --bootstrap-constraints tags=juju maas-one maas-one && juju add-model --config default-series=bionic openstack
 # For ubuntu 20.04 deployment
