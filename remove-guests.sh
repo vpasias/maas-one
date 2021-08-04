b00cb98bf1ab213367a8c69a297169f63dce4a0b
vpasias <52623837+vpasias@users.noreply.github.com>
2021-08-04 11:45:26 +0300

    Update remove-guests.sh

--- remove-guests.sh
+++ remove-guests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for i in maas controller node1 node2 node3 node4 node5; do
+for i in maas controller node1 node2 node3 node4 node5 node6 node7 node8 node9 node10 node11 node12; do
         virsh destroy $i
         virsh undefine $i --remove-all-storage
 done
