fac91c5f42a09dd407cfcdbfffb9b28ac24aa752
vpasias <52623837+vpasias@users.noreply.github.com>
2021-08-12 09:34:21 +0300

    Update 00500-deploy-ceph-mon.sh

--- os/00500-deploy-ceph-mon.sh
+++ os/00500-deploy-ceph-mon.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-juju deploy --config config/ceph-mon.yaml -n 3 --to lxd:0,lxd:1,lxd:2 cs:ceph-mon ceph-mon
+# juju deploy --config config/ceph-mon.yaml -n 3 --to lxd:0,lxd:1,lxd:2 cs:ceph-mon ceph-mon
+juju deploy --config config/ceph-mon.yaml -n 3 --to lxd:0,lxd:1,lxd:2 cs:~openstack-charmers-next/ceph-mon ceph-mon
 juju add-relation ceph-mon:osd ceph-osd:mon
