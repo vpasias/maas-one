b50f6d94eaccbaf06011a5b250ebca30bdc1761c
vpasias <52623837+vpasias@users.noreply.github.com>
2021-08-12 09:44:39 +0300

    Update 00900-deploy-mysql.sh

--- os/00900-deploy-mysql.sh
+++ os/00900-deploy-mysql.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-juju deploy --config config/mysql.yaml -n 3 --to lxd:0,lxd:1,lxd:2 cs:percona-cluster mysql
-juju deploy --config config/mysql.yaml cs:hacluster mysql-hacluster
+# juju deploy --config config/mysql.yaml -n 3 --to lxd:0,lxd:1,lxd:2 cs:percona-cluster mysql
+juju deploy --config config/mysql.yaml -n 3 --to lxd:0,lxd:1,lxd:2 cs:~openstack-charmers-next/percona-cluster mysql
+# juju deploy --config config/mysql.yaml cs:hacluster mysql-hacluster
+juju deploy --config config/mysql.yaml cs:~openstack-charmers-next/hacluster mysql-hacluster
 juju add-relation mysql:ha mysql-hacluster:ha
