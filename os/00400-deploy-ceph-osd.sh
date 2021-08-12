05ff8a3b569fe9d54b8a3fcbaa5b45a6ab4b4b4b
vpasias <52623837+vpasias@users.noreply.github.com>
2021-08-12 09:29:04 +0300

    Update 00400-deploy-ceph-osd.sh

--- os/00400-deploy-ceph-osd.sh
+++ os/00400-deploy-ceph-osd.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-juju deploy --config config/ceph-osd.yaml -n 3 --to 6,7,8 cs:ceph-osd ceph-osd
+#juju deploy --config config/ceph-osd.yaml -n 3 --to 6,7,8 cs:ceph-osd ceph-osd
+juju deploy --config config/ceph-osd.yaml -n 3 --to 6,7,8 cs:~openstack-charmers-next/ceph-osd ceph-osd
