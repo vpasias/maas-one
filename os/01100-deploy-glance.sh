4dda46ba3aa477d32c01aa5b4d1f2a3300b4131f
vpasias <52623837+vpasias@users.noreply.github.com>
2021-08-12 10:45:25 +0300

    Update 01100-deploy-glance.sh

--- os/01100-deploy-glance.sh
+++ os/01100-deploy-glance.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-juju deploy --config config/glance.yaml -n 3 --to lxd:0,lxd:1,lxd:2 cs:glance glance
-juju deploy --config config/glance.yaml cs:hacluster glance-hacluster
+#juju deploy --config config/glance.yaml -n 3 --to lxd:0,lxd:1,lxd:2 cs:glance glance
+#juju deploy --config config/glance.yaml cs:hacluster glance-hacluster
+juju deploy --config config/glance.yaml -n 3 --to lxd:0,lxd:1,lxd:2 cs:~openstack-charmers-next/glance glance
+juju deploy --config config/glance.yaml cs:~openstack-charmers-next/hacluster glance-hacluster
 juju add-relation glance:ha glance-hacluster:ha
 #
 juju add-relation glance:shared-db mysql:shared-db
