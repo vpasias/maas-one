ce3abd3dfe0f8884ca08d3d88fd0f450c3307ac9
vpasias <52623837+vpasias@users.noreply.github.com>
2021-08-12 11:28:09 +0300

    Update 01450-deploy-placement.sh

--- os/01450-deploy-placement.sh
+++ os/01450-deploy-placement.sh
@@ -3,6 +3,6 @@ juju deploy --config config/placement.yaml -n 3 --to lxd:0,lxd:1,lxd:2 cs:~opens
 juju deploy --config config/placement.yaml cs:~openstack-charmers-next/hacluster placement-hacluster
 juju add-relation placement:ha placement-hacluster:ha
 #
+juju add-relation placement:placement nova-cloud-controller:placement
 juju add-relation placement:shared-db mysql:shared-db
 juju add-relation placement:identity-service keystone:identity-service
-juju add-relation placement:placement nova-cloud-controller:placement
