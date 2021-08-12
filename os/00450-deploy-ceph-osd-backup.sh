cb130e6f63c8a1f92434be7e62a94b1646ad9783
vpasias <52623837+vpasias@users.noreply.github.com>
2021-08-12 09:32:39 +0300

    Update 00450-deploy-ceph-osd-backup.sh

--- os/00450-deploy-ceph-osd-backup.sh
+++ os/00450-deploy-ceph-osd-backup.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-juju deploy --config config/ceph-osd-backup.yaml -n 3 --to 9,10,11 cs:ceph-osd ceph-osd-backup
+#juju deploy --config config/ceph-osd-backup.yaml -n 3 --to 9,10,11 cs:ceph-osd ceph-osd-backup
+juju deploy --config config/ceph-osd-backup.yaml -n 3 --to 9,10,11 cs:~openstack-charmers-next/ceph-osd ceph-osd-backup
