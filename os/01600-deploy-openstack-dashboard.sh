20736c27e465b917a4c6913f878f37ef10564b45
vpasias <52623837+vpasias@users.noreply.github.com>
2021-08-12 11:05:16 +0300

    Update 01600-deploy-openstack-dashboard.sh

--- os/01600-deploy-openstack-dashboard.sh
+++ os/01600-deploy-openstack-dashboard.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-juju deploy --config config/openstack-dashboard.yaml -n 3 --to lxd:0,lxd:1,lxd:2 cs:openstack-dashboard openstack-dashboard
-juju deploy --config config/openstack-dashboard.yaml cs:hacluster openstack-dashboard-hacluster
+#juju deploy --config config/openstack-dashboard.yaml -n 3 --to lxd:0,lxd:1,lxd:2 cs:openstack-dashboard openstack-dashboard
+#juju deploy --config config/openstack-dashboard.yaml cs:hacluster openstack-dashboard-hacluster
+juju deploy --config config/openstack-dashboard.yaml -n 3 --to lxd:0,lxd:1,lxd:2 cs:~openstack-charmers-next/openstack-dashboard openstack-dashboard
+juju deploy --config config/openstack-dashboard.yaml cs:~openstack-charmers-next/hacluster openstack-dashboard-hacluster
 juju add-relation openstack-dashboard:ha openstack-dashboard-hacluster:ha
 #
 juju add-relation openstack-dashboard:shared-db mysql:shared-db
