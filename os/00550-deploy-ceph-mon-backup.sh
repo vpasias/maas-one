2f82744f13ec31e55daab60fe1b1c12ae1f58a10
vpasias <52623837+vpasias@users.noreply.github.com>
2021-08-12 09:39:21 +0300

    Update 00550-deploy-ceph-mon-backup.sh

--- os/00550-deploy-ceph-mon-backup.sh
+++ os/00550-deploy-ceph-mon-backup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-juju deploy --config config/ceph-mon-backup.yaml -n 3 --to lxd:0,lxd:1,lxd:2 cs:ceph-mon ceph-mon-backup
+# juju deploy --config config/ceph-mon-backup.yaml -n 3 --to lxd:0,lxd:1,lxd:2 cs:ceph-mon ceph-mon-backup
+juju deploy --config config/ceph-mon-backup.yaml -n 3 --to lxd:0,lxd:1,lxd:2 cs:~openstack-charmers-next/ceph-mon ceph-mon-backup
 juju add-relation ceph-mon-backup:osd ceph-osd-backup:mon
