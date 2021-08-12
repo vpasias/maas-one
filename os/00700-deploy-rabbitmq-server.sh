2f237875b7ac0a3e0ecdc7d26d049676220c5c89
vpasias <52623837+vpasias@users.noreply.github.com>
2021-08-12 09:45:11 +0300

    Update 00700-deploy-rabbitmq-server.sh

--- os/00700-deploy-rabbitmq-server.sh
+++ os/00700-deploy-rabbitmq-server.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+# juju deploy -n 3 --to lxd:0,lxd:1,lxd:2 cs:rabbitmq-server rabbitmq-server
 juju deploy -n 3 --to lxd:0,lxd:1,lxd:2 cs:~openstack-charmers-next/rabbitmq-server rabbitmq-server
