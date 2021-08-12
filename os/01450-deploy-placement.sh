7106e8c68cd054912c353a70d2bd9e7197dcb231
vpasias <52623837+vpasias@users.noreply.github.com>
2021-08-12 11:23:16 +0300

    Create 01450-deploy-placement.sh

---
+++ os/01450-deploy-placement.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+juju deploy --config config/placement.yaml -n 3 --to lxd:0,lxd:1,lxd:2 cs:~openstack-charmers-next/bionic/placement placement
+juju deploy --config config/placement.yaml cs:~openstack-charmers-next/hacluster placement-hacluster
+juju add-relation placement:ha placement-hacluster:ha
+#
+juju add-relation placement:shared-db mysql:shared-db
+juju add-relation placement:identity-service keystone:identity-service
+juju add-relation placement:placement nova-cloud-controller:placement
